951600101d063c8678de4e485d3d41c275e79ab1
micheleTummolo <tummolomichele02@gmail.com>
2022-12-14 21:16:20 +0100

    Done carousel with Bonus 1

--- js/script.js
+++ js/script.js
@@ -32,21 +32,36 @@ let image = document.getElementsByClassName('image')
 image[0].classList.remove('inactive')
 image[0].classList.add('active')
 
+let thumb = document.getElementsByClassName('thumbnail')
+thumb[0].classList.add('border_active')
+
+let dark = document.getElementsByClassName('dark')
+dark[0].classList.remove('active')
+dark[0].classList.add('inactive')
+
 let nextButton = document.getElementById('next')
 let prevButton = document.getElementById('prev')
 let i = 0
+
 
 nextButton.addEventListener ('click', function(){
     console.log("Da: " + i)
     if (i < image.length) {
         image[i].classList.remove('active')
-        image[i].classList.add('inactive') 
+        image[i].classList.add('inactive')
+        dark[i].classList.remove('inactive')
+        dark[i].classList.add('active') 
+        thumb[i].classList.remove('border_active')
         i++
         if (i == image.length) {
             i=0
         }
         image[i].classList.remove('inactive')
         image[i].classList.add('active')
+        dark[i].classList.remove('active')
+        dark[i].classList.add('inactive') 
+        thumb[i].classList.add('border_active')
+
     }
     console.log("A: " + i)
 })
@@ -55,19 +70,27 @@ prevButton.addEventListener ('click', function(){
     console.log("Da: " + i)
     if (i < image.length) {
         image[i].classList.remove('active')
-        image[i].classList.add('inactive') 
+        image[i].classList.add('inactive')
+        dark[i].classList.remove('inactive')
+        dark[i].classList.add('active') 
+        thumb[i].classList.remove('border_active')
         if (i == 0) {
             i = image.length
         }
         i--
         image[i].classList.remove('inactive')
         image[i].classList.add('active')
+        dark[i].classList.remove('active')
+        dark[i].classList.add('inactive')
+        thumb[i].classList.add('border_active')
     }
     console.log("A: " + i)
 })
 
 function imageCreator(arrayLength) {
     let  imagesContainer = document.getElementById('img_container')
+
+    let thumbContainer = document.getElementById('thumb_container')
 
     for (let i = 0; i < arrayLength; i++) {
         imagesContainer.innerHTML += `
@@ -79,6 +102,11 @@ function imageCreator(arrayLength) {
             </div>  
         </div>`
 
+        thumbContainer.innerHTML += `
+        <div class="thumbnail">
+            <img src="./img/${images[i].image}" alt="">
+            <div class="dark active"></div>
+        </div>`
     }
 
 }
